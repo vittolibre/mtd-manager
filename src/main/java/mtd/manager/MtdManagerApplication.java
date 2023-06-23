457320bd87f84633b5ecbdafd3270a58eec7cb1f
vittolibre <vittorio.libretti.ing@gmail.com>
2023-06-23 10:47:06 +0200

    first commit

---
+++ src/main/java/mtd/manager/MtdManagerApplication.java
@@ -0,0 +1,13 @@
+package mtd.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtdManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtdManagerApplication.class, args);
+    }
+
+}
